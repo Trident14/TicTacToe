24e1f100dde35009dfb4b35108954c09a294117a
Trident14 <sekarinesh14@gmail.com>
2023-07-18 14:21:46 +0530

    Initial Commit

---
+++ TicTakToe/TicTakToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTakToeApp.swift
+//  TicTakToe
+//
+//  Created by Inesh Sekar on 18/07/23.
+//
+
+import SwiftUI
+
+@main
+struct TicTakToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
